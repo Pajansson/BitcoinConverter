9c5358080ebd65b8795ce6eb8da3a5ef0c185ff6
Patrik Jansson <Patrik_jansson@hotmail.com>
2018-01-04 15:10:11 +0100

    Remove usings

--- BitcoinConverter/BitcoinConverter/App.xaml.cs
+++ BitcoinConverter/BitcoinConverter/App.xaml.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.AppCenter;
+﻿using Microsoft.AppCenter;
 using Microsoft.AppCenter.Analytics;
 using Microsoft.AppCenter.Crashes;
 using Xamarin.Forms;
